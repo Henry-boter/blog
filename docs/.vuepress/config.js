fa0f3552a5fe8490d5fa83bb61d653bf5f5576bf
hebo <1971785019@qq.com>
2019-12-24 15:03:22 +0800

    基础布局

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // theme:'reform',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    logo: 'https://i.loli.net/2019/12/19/Lh7ZSUCJX2Kon3j.jpg',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Github', link: '/' },
+    ]
+  }
+}
