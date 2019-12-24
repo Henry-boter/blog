0a68a7555818eb3d8127402228467a84c9e00c0d
hebo <1971785019@qq.com>
2019-12-24 21:35:54 +0800

    pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,43 @@
 module.exports = {
-  // theme:'reform',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: '翌擎大前端',
+  description: '技术文档',
   themeConfig: {
     logo: 'https://i.loli.net/2019/12/19/Lh7ZSUCJX2Kon3j.jpg',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      },
       { text: 'Github', link: '/' },
-    ]
+      { text: '关注我', link: 'https://google.com', target:'_self', rel:'' },
+    ],
+    sidebar: [
+      '/',
+      '/guide/',
+      ['/', 'Explicit link text']
+    ],
+    // sidebar: [
+    //   {
+    //     title: 'Group 1',   // 必要的
+    //     path: '/foo/',      // 可选的, 应该是一个绝对路径
+    //     collapsable: false, // 可选的, 默认值是 true,
+    //     sidebarDepth: 1,    // 可选的, 默认值是 1
+    //     children: [
+    //       '/'
+    //     ]
+    //   },
+    //   {
+    //     title: 'Group 2',
+    //     children: [ /* ... */ ]
+    //   }
+    // ]
+    lastUpdated: 'Last Updated', // string | boolean
+    smoothScroll: true
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
+heroImage: https://i.loli.net/2019/12/19/Lh7ZSUCJX2Kon3j.jpg
+heroText: Engine
+tagline: null
 actionText: 快速上手 →
-actionLink: /zh/guide/
+actionLink: /guide/
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
